bf9d93caccedc12d9458a5406aa235817b62c96e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-17 23:57:04 +0600

    js

---
+++ index-1.js
@@ -0,0 +1,11 @@
+// javascript {
+
+// * arrow functions ES5*
+function addition(a, b) {
+  return a + b;
+}
+console.log(addition(2,3));
+
+// * es6 arrow functions
+const additions = (a, b) => a + b;
+console.log(additions(2,3));
