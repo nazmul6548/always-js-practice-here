a81f8d40f1548e88f155f928e85d81ae21ba954d
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-19 18:44:36 +0600

    concatenation

--- index-2.js
+++ index-2.js
@@ -36,3 +36,21 @@ const name =[
 const copyarr=[...name]
 copyarr.splice(copyarr.length,1 ,"jewel")
 console.log(copyarr);
+
+
+// rest operator
+// const arr1=[1,2,3,4];
+// const arr2=[5,7,4,3];
+// const arr3=[6,5,6];
+
+// const totalarr=[...arr1,...arr2,...arr3];
+// console.log(totalarr);
+
+// same as concatenation operator.
+// const arr1=[1,2,3,4];
+// const arr2=[5,7,4,3];
+// const arr3=[6,5,6];
+// const totalarr =arr1 + arr2 + arr3;
+// console.log(totalarr); 
+// It does not return an array
+
