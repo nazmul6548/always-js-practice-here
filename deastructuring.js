0d727396f8606e73d0124ebe856c276600409247
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-19 20:20:20 +0600

    deastructuring

---
+++ deastructuring.js
@@ -0,0 +1,12 @@
+// deastructuring for arrays
+
+
+
+
+const myName =["nazmul", "jewel", "islam"];
+
+const [firstName, lastName,middleName] = myName;
+
+console.log(firstName,middleName, lastName);
+
+
