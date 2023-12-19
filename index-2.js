961193e52c6c4699befa36fb1ca7b323fcd2616e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-19 18:51:12 +0600

    concat

--- index-2.js
+++ index-2.js
@@ -39,12 +39,13 @@ console.log(copyarr);
 
 
 // rest operator
-// const arr1=[1,2,3,4];
-// const arr2=[5,7,4,3];
-// const arr3=[6,5,6];
+const arr1=[1,2,3,4];
+const arr2=[5,7,4,3];
+const arr3=[6,5,6];
 
-// const totalarr=[...arr1,...arr2,...arr3];
-// console.log(totalarr);
+const totalarr=[...arr1,...arr2,...arr3];
+console.log(totalarr);
+// return array
 
 // same as concatenation operator.
 // const arr1=[1,2,3,4];
@@ -54,3 +55,12 @@ console.log(copyarr);
 // console.log(totalarr); 
 // It does not return an array
 
+
+
+// concat method
+// const arr1=[1,2,3,4];
+// const arr2=[5,7,4,3];
+// const arr3=[6,5,6];
+// const totalarr = arr1.concat(arr2,arr3);
+// console.log(totalarr);
+// return array
