1de1c071dd8409b6a3a08d91d91ab00502ccac4c
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-19 12:06:57 +0600

    problem

---
+++ index-2.js
@@ -0,0 +1,23 @@
+// const name =[
+//     "jakaria",
+//     "maria",
+//     "mars",
+//     "jupiter",
+//     "solar",
+//     "earth"
+// ]
+// const copyArr=[...name];
+// name.splice(2,3 ,"jewel","nazmul");
+// console.log(name);
+
+
+// const name =[
+//     "jakaria",
+//     "maria",
+//     "mars",
+//     "jupiter",
+//     "solar",
+//     "earth"
+// ]
+
+// console.log(name);
