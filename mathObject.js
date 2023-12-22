fd3fd272c0f7b2015ac885b1bb0ea0dbbeb4704e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-22 18:15:44 +0600

    round

--- mathObject.js
+++ mathObject.js
@@ -2,4 +2,12 @@
 // 1.properties : 
 // example 
 console.log(Math.PI);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
 // 2.mathods
+// example 
+// math.round
+const result = Math.round(3.555);
+console.log(result);
+const result1 = Math.round(9.99991);
+console.log(result1);
