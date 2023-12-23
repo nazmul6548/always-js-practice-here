b6999770c6512fc854dfe16c76e642574a181c27
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-23 22:07:55 +0600

    date

---
+++ date.js
@@ -0,0 +1,13 @@
+// date object
+
+
+// without arguments
+const curretDate = new Date();
+console.log(curretDate);
+
+// with date string
+const dateString = '2022-03-16'
+const currentdate = new Date(dateString);
+console.log(currentdate);
+
+// 
