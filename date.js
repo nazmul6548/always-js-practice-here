3591951cd8768c0bcb996097620dba9db8687c2e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-23 22:28:56 +0600

    date

--- date.js
+++ date.js
@@ -10,4 +10,34 @@ const dateString = '2022-03-16'
 const currentdate = new Date(dateString);
 console.log(currentdate);
 
-// 
+// with year month day
+const fixedDate = new Date(2023,11,15);
+console.log(fixedDate);
+
+// with milisecond
+const milisecond = 1875555554875;
+const dateFromMilliseconds = new Date(milisecond)
+console.log(dateFromMilliseconds);
+
+// date component
+const now = new Date();
+const year = now.getFullYear();
+console.log(year);
+
+
+
+const now1 = new Date();
+const month = now1.getMonth();
+console.log(month);
+
+
+
+const now2 = new Date();
+const day = now2.getDay();
+console.log(day);
+
+// todatestring
+const now3 = new Date();
+console.log(now3.toDateString());
+
+// totimestring
