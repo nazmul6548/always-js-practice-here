dfd730d5ed5be031218c10a5a7d4229618a69d74
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-29 18:46:41 +0600

    while loop

--- while vs for loop.js
+++ while vs for loop.js
@@ -1,6 +1,13 @@
 // for loop
 
-for (let inde = 0; inde <= 40; inde+=3) {
+// for (let inde = 0; inde <= 40; inde+=3) {
     
-    console.log(inde);
+//     console.log(inde);
+// }
+
+// while looop
+let count = 0;
+while (count <= 40) {
+    console.log(count);
+    count=count+2;
 }
