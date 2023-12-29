0b78374bd052f9a73c86d5a3c1f1c5647ce36c47
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-29 21:49:48 +0600

    loop

--- while vs for loop.js
+++ while vs for loop.js
@@ -6,8 +6,20 @@
 // }
 
 // while looop
-let count = 0;
-while (count <= 40) {
-    console.log(count);
-    count=count+2;
+// const count = 0;
+// while (count <= 40) {
+//     console.log(count);
+//     count=count+2;
+// }
+
+
+// for loop with condition in array
+let name =["nazmul","islam","jewel","lamia","fuhad"];
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+    if (i ===2) {
+       
+        break;
+    }
+    
 }
