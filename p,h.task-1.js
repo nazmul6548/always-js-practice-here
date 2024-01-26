0be7a8b5a1beed3f5a0b2f8ee377878f05e0a4dd
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-26 09:43:27 +0600

    js problem solve

---
+++ p,h.task-1.js
@@ -0,0 +1,12 @@
+// *******************************
+// Task-1
+// You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+// *******************************
+
+// ans:
+var iHaveMoney = 1000;
+var apples1kg =300;
+var oranges1kg = 400;
+var totalCost = apples1kg + oranges1kg;
+var backMe_money = iHaveMoney - totalCost;
+console.log(backMe_money);
