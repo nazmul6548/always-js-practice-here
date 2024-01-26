84facfab111b6ecb11811c9849c91ac71b2361bd
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-26 09:53:37 +0600

    task2

---
+++ p,h.task-2.js
@@ -0,0 +1,13 @@
+// ******************************
+// Task-2
+// Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+// ******************************
+// ans:
+var a_student_mark_of_mathematics = 98;
+var a_student_mark_of_biology = 89;
+var a_student_mark_of_chemistry = 76;
+var a_student_mark_of_physics = 90;
+var a_student_mark_of_bangla = 88;
+
+var average_mark = a_student_mark_of_mathematics + a_student_mark_of_chemistry + a_student_mark_of_biology + a_student_mark_of_physics + a_student_mark_of_bangla;
+console.log(average_mark);
