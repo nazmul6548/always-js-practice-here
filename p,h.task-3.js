7a6d94a7b375cd6f83c143ff8c3f912f7771ec7e
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-01-26 10:09:56 +0600

    task3

---
+++ p,h.task-3.js
@@ -0,0 +1,8 @@
+// *********************************
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
+// *********************************
+// ans:
+var number = 999;
+var showTheRemainder = number % 5;
+console.log(showTheRemainder);
